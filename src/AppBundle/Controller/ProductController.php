d8c7303628bb56a54aca9277b72925ed846c8744
Jeroen Ketelaar <jeroen@ketelaar.me>
2018-09-23 23:06:56 -0500

    [FEATURE] Added Product controller
    - Display all
    - Edit
    - Remove
    - View

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use AppBundle\Service\SerializerManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Product controller.
+ *
+ * @Route("product")
+ */
+class ProductController extends Controller
+{
+    /**
+     * Lists all product entities.
+     *
+     * @Route("/", name="product_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        return new JsonResponse(SerializerManager::normalize($products));
+    }
+
+    /**
+     * Creates a new product entity.
+     *
+     * @Route("/new", name="product_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm('AppBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
+        }
+
+        return $this->render(
+            'product/new.html.twig',
+            [
+                'product' => $product,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Finds and displays a product entity.
+     *
+     * @Route("/{id}", name="product_show")
+     * @Method("GET")
+     */
+    public function showAction(Product $product)
+    {
+        return new JsonResponse(SerializerManager::normalize($product));
+    }
+
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     * @Route("/{id}/edit", name="product_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AppBundle\Form\ProductType', $product);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render(
+            'product/edit.html.twig',
+            [
+                'product' => $product,
+                'edit_form' => $editForm->createView(),
+                'delete_form' => $deleteForm->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', ['id' => $product->getId()]))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+
+    /**
+     * Deletes a product entity.
+     *
+     * @Route("/{id}", name="product_delete")
+     * @Method("DELETE")
+     *
+     * @param Request $request
+     * @param Product $product
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $form = $this->createDeleteForm($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_index');
+    }
+}
